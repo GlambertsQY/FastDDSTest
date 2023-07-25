0290f3cd4c507e98e7a74f28b94421be46eb0142
GlambertsQY <yyangwwhh1221@hotmail.com>
2023-07-25 21:25:52 +0800

    精简优化

--- Source/FastDDSTest/Private/MyInfo.cpp
+++ Source/FastDDSTest/Private/MyInfo.cpp
@@ -3,120 +3,15 @@
 
 #include "MyInfo.h"
 
-// #include "ObjectClassTest.h"
-
-// #include "LocationRotationSubscriber.h"
-
-
-// #include "LocationRotationPubSubTypes.h"
-//
-// #include <fastdds/dds/domain/DomainParticipantFactory.hpp>
-// #include <fastdds/dds/subscriber/DataReader.hpp>
-// #include <fastdds/dds/subscriber/SampleInfo.hpp>
-// #include <fastdds/dds/subscriber/Subscriber.hpp>
-// #include <fastdds/dds/subscriber/qos/DataReaderQos.hpp>
-
-
-// using namespace eprosima::fastdds::dds;
-
 AMyInfo::AMyInfo()
 {
-	// participant_ = nullptr;
-	// subscriber_ = nullptr;
-	// topic_ = nullptr;
-	// reader_ = nullptr;
-	// type_ = new TypeSupport(new LocationRotationBeanPubSubType());
 	PrimaryActorTick.bCanEverTick = true;
 }
-
-// bool AMyInfo::init()
-// {
-// 	//CREATE THE PARTICIPANT
-// 	DomainParticipantQos pqos;
-// 	pqos.name("FastDDSTestUE");
-// 	participant_ = DomainParticipantFactory::get_instance()->create_participant(0, pqos);
-// 	if (participant_ == nullptr)
-// 	{
-// 		return false;
-// 	}
-//
-// 	//REGISTER THE TYPE
-// 	//bUseRTTI = true;
-// 	type_->register_type(participant_);
-//
-// 	//CREATE THE SUBSCRIBER
-// 	subscriber_ = participant_->create_subscriber(SUBSCRIBER_QOS_DEFAULT, nullptr);
-// 	if (subscriber_ == nullptr)
-// 	{
-// 		return false;
-// 	}
-//
-// 	//CREATE THE TOPIC
-// 	topic_ = participant_->create_topic(
-// 		"myTopic",
-// 		type_->get_type_name(),
-// 		TOPIC_QOS_DEFAULT);
-// 	if (topic_ == nullptr)
-// 	{
-// 		return false;
-// 	}
-//
-// 	//CREATE THE READER
-// 	DataReaderQos rqos = DATAREADER_QOS_DEFAULT;
-// 	rqos.reliability().kind = RELIABLE_RELIABILITY_QOS;
-// 	reader_ = subscriber_->create_datareader(topic_, rqos, &listener_);
-// 	if (reader_ == nullptr)
-// 	{
-// 		return false;
-// 	}
-// 	return true;
-// }
-//
-// void AMyInfo::SubListener::on_data_available(eprosima::fastdds::dds::DataReader* reader)
-// {
-// 	// Take data
-// 	LocationRotationBean st;
-// 	SampleInfo info;
-//
-// 	if (reader->take_next_sample(&st, &info) == ReturnCode_t::RETCODE_OK)
-// 	{
-// 		if (info.valid_data)
-// 		{
-// 			// Print your structure data here.
-// 			++samples;
-// 			FString s = st.LRJsonString().c_str();
-// 			GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Red, s, true);
-// 			std::cout << "Sample received, count=" << samples << std::endl;
-// 		}
-// 	}
-// }
-
-// void AMyInfo::SubListener::on_subscription_matched(eprosima::fastdds::dds::DataReader* reader,
-//                                                    const eprosima::fastdds::dds::SubscriptionMatchedStatus& info)
-// {
-// 	if (info.current_count_change == 1)
-// 	{
-// 		matched = info.total_count;
-// 		std::cout << "Subscriber matched." << std::endl;
-// 	}
-// 	else if (info.current_count_change == -1)
-// 	{
-// 		matched = info.total_count;
-// 		std::cout << "Subscriber unmatched." << std::endl;
-// 	}
-// 	else
-// 	{
-// 		std::cout << info.current_count_change
-// 			<< " is not a valid value for SubscriptionMatchedStatus current count change" << std::endl;
-// 	}
-// }
 
 void AMyInfo::BeginPlay()
 {
 	Super::BeginPlay();
-	// mysub = GetWorld()->SpawnActor<ALRSubscriberActor>(ALRSubscriberActor::StaticClass(), FVector::ZeroVector, FRotator::ZeroRotator);
 	mysub = NewObject<UJsonStrSubscriber>();
-	// mysub = NewObject<UObjectClassTest>();
 	if(mysub->init())
 	{
 		GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Red, "mysub Initialed and run", true);
@@ -127,34 +22,6 @@ void AMyInfo::BeginPlay()
 	{
 		GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Red, "cppSub Initialed and run", true);
 	}
-	// LocationRotationSubscriber mysub;
-	// if (mysub.init())
-	// {
-	// 	mysub.run();
-	// 	// run(count, sleep);
-	// 	GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Red, "Initialed and run", true);
-	// }
-	// if (init())
-	// {
-	// 	GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Red, "Initialed and run", true);
-	// }
-}
-
-void AMyInfo::EndPlay(const EEndPlayReason::Type EndPlayReason)
-{
-	// if (reader_ != nullptr)
-	// {
-	// 	subscriber_->delete_datareader(reader_);
-	// }
-	// if (topic_ != nullptr)
-	// {
-	// 	participant_->delete_topic(topic_);
-	// }
-	// if (subscriber_ != nullptr)
-	// {
-	// 	participant_->delete_subscriber(subscriber_);
-	// }
-	// DomainParticipantFactory::get_instance()->delete_participant(participant_);
 }
 
 void AMyInfo::Tick(float DeltaSeconds)
--- Source/FastDDSTest/Public/MyInfo.h
+++ Source/FastDDSTest/Public/MyInfo.h
@@ -6,15 +6,9 @@
 #pragma warning(disable : 4265)
 
 
-// #include <fastdds/dds/domain/DomainParticipant.hpp>
-// #include <fastdds/dds/subscriber/DataReader.hpp>
-// #include <fastdds/dds/subscriber/DataReaderListener.hpp>
-// #include <fastdds/dds/subscriber/Subscriber.hpp>
-// #include "FastDDSUEPlugin/Public/LocationRotationSubscriber.h"
-#include "..\..\..\Plugins\FastDDSUEPlugin\Source\FastDDSUEPlugin\Public\JsonStrSubscriber.h"
+#include "JsonStrSubscriber.h"
 
 #include "CoreMinimal.h"
-// #include "LRSubscriberActor.h"
 #include "GameFramework/Info.h"
 #include "MyInfo.generated.h"
 
@@ -34,47 +28,10 @@ public:
 
 	UPROPERTY()
 	UJsonStrSubscriber* cppSub;
-	 
-	// bool init();
-	
-
-
-private:
-
-	// UPROPERTY()
-	// UObjectClassTest* mysub;
-	// LocationRotationSubscriber mysub;
-// 	eprosima::fastdds::dds::DomainParticipant* participant_;
-// 	eprosima::fastdds::dds::Subscriber* subscriber_;
-// 	eprosima::fastdds::dds::Topic* topic_;
-// 	eprosima::fastdds::dds::DataReader* reader_;
-// 	eprosima::fastdds::dds::TypeSupport* type_;
-//
-// 	class SubListener : public eprosima::fastdds::dds::DataReaderListener
-// 	{
-// 	public:
-//
-// 		SubListener() = default;
-//
-// 		~SubListener() override = default;
-//
-// 		void on_data_available(
-// 				eprosima::fastdds::dds::DataReader* reader) override;
-//
-// 		void on_subscription_matched(
-// 				eprosima::fastdds::dds::DataReader* reader,
-// 				const eprosima::fastdds::dds::SubscriptionMatchedStatus& info) override;
-//
-// 		int matched = 0;
-// 		uint32_t samples = 0;
-// 	}
-// 	listener_;
 
 
 protected:
 	virtual void BeginPlay() override;
-
-	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
 
 	virtual void Tick(float DeltaSeconds) override;
 };
