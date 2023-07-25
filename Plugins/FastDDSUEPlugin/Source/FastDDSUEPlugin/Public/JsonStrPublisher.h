346188d80541a5ae85d240e5f447858b2641e428
GlambertsQY <yyangwwhh1221@hotmail.com>
2023-07-25 17:10:50 +0800

    新建Publisher文件

---
+++ Plugins/FastDDSUEPlugin/Source/FastDDSUEPlugin/Public/JsonStrPublisher.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "JsonStrPublisher.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FASTDDSUEPLUGIN_API UJsonStrPublisher : public UObject
+{
+	GENERATED_BODY()
+};
